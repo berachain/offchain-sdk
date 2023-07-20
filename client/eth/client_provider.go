174e949c030ee947db2ec8ba26c620533752c55d
ocnc <anocpublic@gmail.com>
2023-07-20 13:22:45 -0400

    bing bong

--- client/eth/client_provider.go
+++ client/eth/client_provider.go
@@ -3,15 +3,11 @@ package eth
 import (
 	"context"
 	"errors"
-	"fmt"
 	"math/big"
-	"sync"
-	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	lru "github.com/hashicorp/golang-lru/v2"
 )
 
 var (
@@ -21,85 +17,15 @@ var (
 type ChainProvider interface {
 	Reader
 	Writer
-	GetChainClient(string) (Client, bool)
-	GetAnyChainClient() (Client, bool)
-	AddChainClient(Config) bool
-	RemoveChainClient(string) error
+	ConnectionPool
 }
 
 type ChainProviderImpl struct {
-	cache  *lru.Cache[string, Client]
-	mutex  sync.Mutex
-	config ChainProviderConfig
+	ConnectionPool
 }
 
-type ChainProviderConfig struct {
-	cacheSize      uint
-	defaultTimeout time.Duration
-}
-
-func NewChainProviderImpl(cfg ChainProviderConfig) (ChainProvider, error) {
-	cache, err := lru.NewWithEvict[string, Client](int(cfg.cacheSize), func(_ string, v Client) {
-		defer v.Close()
-		// The timeout is added so that any in progress requests have a chance to complete before we close.
-		time.Sleep(cfg.defaultTimeout)
-	})
-	if err != nil {
-		return nil, err
-	}
-	return &ChainProviderImpl{
-		cache:  cache,
-		config: cfg,
-	}, nil
-}
-
-// GetChainClient returns a chain client from the cache.
-// If clientAddr isn't specified, it returns any client from the cache.
-func (c *ChainProviderImpl) GetChainClient(clientAddr string) (Client, bool) {
-	// If a clientAddr is not specified, return any from the pool.
-	if clientAddr == "" {
-		return c.GetAnyChainClient()
-	}
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	return c.cache.Get(clientAddr)
-}
-
-func (c *ChainProviderImpl) GetAnyChainClient() (Client, bool) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	_, client, ok := c.cache.GetOldest()
-	return client, ok
-}
-
-func (c *ChainProviderImpl) AddChainClient(cfg Config) bool {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	// If replacing, be sure to close old client first.
-	// The LRU cache's eviction policy is not triggered on value updates/replacements.
-	if c.cache.Contains(cfg.EthHTTPURL) {
-		err := c.removeClient(cfg.EthHTTPURL)
-		if err != nil {
-			return false
-		}
-	}
-	client := NewClient(&cfg)
-	return c.cache.Add(cfg.EthHTTPURL, client)
-}
-
-func (c *ChainProviderImpl) RemoveChainClient(clientAddr string) error {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	return c.removeClient(clientAddr)
-}
-
-func (c *ChainProviderImpl) removeClient(clientAddr string) error {
-	client, ok := c.cache.Get(clientAddr)
-	if !ok {
-		return fmt.Errorf("could not get client for: %s", clientAddr)
-	}
-	client.Close()
-	return nil
+func NewChainProviderImpl(pool ConnectionPool) (ChainProvider, error) {
+	return &ChainProviderImpl{pool}, nil
 }
 
 // ==================================================================
