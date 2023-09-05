7e047c548b841d4129acfccccab1ae8fcd6725ed
Devon Bear <itsdevbear@berachain.com>
2023-09-05 13:29:55 -0400

    fix(lint): lint

--- cmd/start.go
+++ cmd/start.go
@@ -57,7 +57,7 @@ func StartCmdWithOptions[C any](app App[C], defaultAppHome string, _ StartCmdOpt
 			ethClient := eth.NewClient(&cfg.Eth)
 			ab.RegisterEthClient(ethClient)
 
-			if err := ethClient.Dial(); err != nil {
+			if err = ethClient.Dial(); err != nil {
 				logger.Error("failed to dial chain node", err)
 			}
 
