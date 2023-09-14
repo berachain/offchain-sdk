4a512bd756e1f107859a37299bc64525d6b9229c
Devon Bear <itsdevbear@berachain.com>
2023-09-14 17:34:35 -0400

    add wanr

--- log/log.go
+++ log/log.go
@@ -13,6 +13,10 @@ type Logger interface {
 	// The key of the tuple must be a string.
 	Info(msg string, keyVals ...any)
 
+	// Warn takes a message and a set of key/value pairs and logs with level WARN.
+	// The key of the tuple must be a string.
+	Warn(msg string, keyVals ...any)
+
 	// Error takes a message and a set of key/value pairs and logs with level ERR.
 	// The key of the tuple must be a string.
 	Error(msg string, keyVals ...any)
