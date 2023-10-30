b7ba6f15ff77d58147a8ed5c49d2144a453cd46a
Devon Bear <itsdevbear@berachain.com>
2023-10-30 11:40:28 -0400

    ree

--- client/eth/client.go
+++ client/eth/client.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	ethcoretypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -25,14 +26,12 @@ type Client interface {
 
 // Reader is the eth reader interface.
 type Reader interface {
-	GetBlockByNumber(ctx context.Context, number uint64) (*ethcoretypes.Block, error)
-	GetReceipts(ctx context.Context, txs ethcoretypes.Transactions) (ethcoretypes.Receipts, error)
-	GetReceipt(ctx context.Context, txHash common.Hash) (*ethcoretypes.Receipt, error)
+	BlockByNumber(ctx context.Context, number *big.Int) (*ethcoretypes.Block, error)
 	SubscribeNewHead(ctx context.Context) (chan *ethcoretypes.Header, ethereum.Subscription, error)
 	BlockNumber(ctx context.Context) (uint64, error)
 	ChainID(ctx context.Context) (*big.Int, error)
 	TransactionReceipt(ctx context.Context, txHash common.Hash) (*ethcoretypes.Receipt, error)
-	GetBalance(ctx context.Context, address common.Address) (*big.Int, error)
+	BalanceAt(ctx context.Context, address common.Address, bn *big.Int) (*big.Int, error)
 	CodeAt(ctx context.Context, account common.Address, blockNumber *big.Int) ([]byte, error)
 	EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64, error)
 	FilterLogs(ctx context.Context, q ethereum.FilterQuery) ([]ethcoretypes.Log, error)
@@ -44,6 +43,7 @@ type Reader interface {
 		ch chan<- ethcoretypes.Log) (ethereum.Subscription, error)
 	SuggestGasPrice(ctx context.Context) (*big.Int, error)
 	SuggestGasTipCap(ctx context.Context) (*big.Int, error)
+	TransactionByHash(ctx context.Context, hash common.Hash) (tx *types.Transaction, isPending bool, err error)
 }
 
 type Writer interface {
@@ -54,9 +54,9 @@ type Writer interface {
 
 // client is the indexer eth client.
 type client struct {
-	cfg        *Config
-	httpclient *ethclient.Client
-	wsclient   *ethclient.Client
+	*ethclient.Client
+	cfg      *Config
+	wsclient *ethclient.Client
 }
 
 // NewClient returns a new client. It has both reader and writer privilege.
@@ -73,7 +73,7 @@ func NewClient(cfg *Config) Client {
 
 // Dial dials the client.
 func (c *client) Dial() error {
-	if c.httpclient != nil || c.wsclient != nil {
+	if c.wsclient != nil {
 		return ErrAlreadyDial
 	}
 	// TODO: manage context better
@@ -85,7 +85,7 @@ func (c *client) Dial() error {
 		retries++
 		httpclient, err = ethclient.DialContext(ctx, c.cfg.EthHTTPURL)
 		if err == nil {
-			c.httpclient = httpclient
+			c.Client = httpclient
 			break
 		}
 		time.Sleep(defaultRetryTime)
@@ -113,10 +113,10 @@ func (c *client) Dial() error {
 
 // Close closes the client.
 func (c *client) Close() error {
-	if c.httpclient == nil || c.wsclient == nil {
+	if c.wsclient == nil {
 		return ErrClosed
 	}
-	c.httpclient.Close()
+	c.Close()
 	c.wsclient.Close()
 	return nil
 }
@@ -125,21 +125,11 @@ func (c *client) Close() error {
 // Client Usage Methods
 // ==================================================================
 
-// GetBlockByNumber returns the block for the given block number.
-func (c *client) GetBlockByNumber(ctx context.Context, number uint64) (*ethcoretypes.Block, error) {
-	return c.httpclient.BlockByNumber(ctx, big.NewInt(int64(number)))
-}
-
-// GetBalance returns the balance for the given address.
-func (c *client) GetBalance(ctx context.Context, address common.Address) (*big.Int, error) {
-	return c.httpclient.BalanceAt(ctx, address, nil)
-}
-
 // GetReceipts returns the receipts for the given transactions.
 func (c *client) GetReceipts(ctx context.Context, txs ethcoretypes.Transactions) (ethcoretypes.Receipts, error) {
 	var receipts ethcoretypes.Receipts
 	for _, tx := range txs {
-		receipt, err := c.httpclient.TransactionReceipt(ctx, tx.Hash())
+		receipt, err := c.TransactionReceipt(ctx, tx.Hash())
 		if err != nil {
 			return nil, err
 		}
@@ -148,73 +138,9 @@ func (c *client) GetReceipts(ctx context.Context, txs ethcoretypes.Transactions)
 	return receipts, nil
 }
 
-// GetReceipt returns the receipt for the given transaction hash.
-func (c *client) GetReceipt(ctx context.Context, txHash common.Hash) (*ethcoretypes.Receipt, error) {
-	return c.httpclient.TransactionReceipt(ctx, txHash)
-}
-
 // SubscribeNewHead subscribes to new block headers.
 func (c *client) SubscribeNewHead(ctx context.Context) (chan *ethcoretypes.Header, ethereum.Subscription, error) {
 	ch := make(chan *ethcoretypes.Header)
 	sub, err := c.wsclient.SubscribeNewHead(ctx, ch)
 	return ch, sub, err
-}
-
-// BlockNumber returns the current block number.
-func (c *client) BlockNumber(ctx context.Context) (uint64, error) {
-	return c.httpclient.BlockNumber(ctx)
-}
-
-// ChainID returns the current chain ID.
-func (c *client) ChainID(ctx context.Context) (*big.Int, error) {
-	return c.httpclient.ChainID(ctx)
-}
-
-func (c *client) TransactionReceipt(ctx context.Context, txHash common.Hash) (*ethcoretypes.Receipt, error) {
-	return c.httpclient.TransactionReceipt(ctx, txHash)
-}
-
-func (c *client) CodeAt(ctx context.Context, contract common.Address, blockNumber *big.Int) ([]byte, error) {
-	return c.httpclient.CodeAt(ctx, contract, blockNumber)
-}
-
-func (c *client) CallContract(ctx context.Context, msg ethereum.CallMsg, blockNumber *big.Int) ([]byte, error) {
-	return c.httpclient.CallContract(ctx, msg, blockNumber)
-}
-
-func (c *client) EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64, error) {
-	return c.httpclient.EstimateGas(ctx, msg)
-}
-
-func (c *client) FilterLogs(ctx context.Context, q ethereum.FilterQuery) ([]ethcoretypes.Log, error) {
-	return c.httpclient.FilterLogs(ctx, q)
-}
-
-func (c *client) HeaderByNumber(ctx context.Context, number *big.Int) (*ethcoretypes.Header, error) {
-	return c.httpclient.HeaderByNumber(ctx, number)
-}
-
-func (c *client) PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error) {
-	return c.httpclient.PendingCodeAt(ctx, account)
-}
-
-func (c *client) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
-	return c.httpclient.PendingNonceAt(ctx, account)
-}
-
-func (c *client) SendTransaction(ctx context.Context, tx *ethcoretypes.Transaction) error {
-	return c.httpclient.SendTransaction(ctx, tx)
-}
-
-func (c *client) SubscribeFilterLogs(ctx context.Context,
-	q ethereum.FilterQuery, ch chan<- ethcoretypes.Log) (ethereum.Subscription, error) {
-	return c.wsclient.SubscribeFilterLogs(ctx, q, ch)
-}
-
-func (c *client) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
-	return c.httpclient.SuggestGasPrice(ctx)
-}
-
-func (c *client) SuggestGasTipCap(ctx context.Context) (*big.Int, error) {
-	return c.httpclient.SuggestGasTipCap(ctx)
 }
