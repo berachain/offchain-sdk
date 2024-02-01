e1e895d5463725f314cac5fb3668f69c10731860
Cal Bera <calbera@berachain.com>
2024-01-31 21:40:01 -0500

    comment

--- core/transactor/config.go
+++ core/transactor/config.go
@@ -8,8 +8,8 @@ type Config struct {
 	Multicall3Address   string
 	TxReceiptTimeout    time.Duration // how long to wait for a tx to be mined
 	PendingNonceTimeout time.Duration // how long to wait for the pending nonce
-	EmtpyQueueDelay     time.Duration // how long to wait if the queue is empty.
-	MaxInFlightBacklog  int
+	EmtpyQueueDelay     time.Duration // how long to wait if the queue is empty
+	MaxInFlightBacklog  int           // how many can accumulate in the inflight queue
 	TxBatchSize         int
 	TxBatchTimeout      time.Duration // how long to wait for a batch to be flushed
 	CallTxTimeout       time.Duration // how long to wait for a eth call result
