9bab8cbce889cffeb43e819de5c9659197f1ec96
Gordon <gordon@Gordons-MacBook-Pro.local>
2024-06-10 16:01:40 -0400

    Fix naming of latency metrics

--- telemetry/prometheus/metrics.go
+++ telemetry/prometheus/metrics.go
@@ -208,7 +208,7 @@ func (p *metrics) Time(name string, value time.Duration, tags []string) {
 
 // Latency is a helper function to measure the latency of a routine.
 func (p *metrics) Latency(jobName string, start time.Time, tags ...string) {
-	p.Time("stats.latency", time.Since(start), append(tags, fmt.Sprintf("job:%s", jobName)))
+	p.Time(fmt.Sprintf("%s.latency", jobName), time.Since(start), tags)
 }
 
 // parseTagsToLabelPairs converts a slice of tags in "key:value" format to two slices:
