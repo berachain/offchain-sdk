b47abafcfff1d6339e3562dfeaa44da36df97162
hunter-bera <133678627+hunter-bera@users.noreply.github.com>
2024-08-07 13:53:25 -0400

    debug makes more sense

--- client/eth/health_client.go
+++ client/eth/health_client.go
@@ -78,7 +78,7 @@ func (c *HealthCheckedClient) StartHealthCheck(ctx context.Context) {
 				c.logger.Error("eth client reporting unhealthy", "err", err, "url", c.dialurl)
 			} else {
 				c.SetHealthy(true)
-				c.logger.Info("eth client reporting healthy", "url", c.dialurl)
+				c.logger.Debug("eth client reporting healthy", "url", c.dialurl)
 			}
 		}
 		time.Sleep(c.healthCheckInterval)
